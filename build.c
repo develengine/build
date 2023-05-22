1d8f75fa381178f217cbf1ccaae5edd8106193f3
Jakub Boroš <xboros1@fi.muni.cz>
2023-05-22 21:03:06 +0200

    Added windows support.

--- build.c
+++ build.c
@@ -1,5 +1,6 @@
 #include "build.h"
 
+
 const char *out = "program";
 
 const char *source_files[] = {
@@ -20,16 +21,22 @@ int main(int argc, char *argv[])
         .optimisations = "2",
 
         .source_files = source_files,
-        .warnings = merge(nice_warnings, extra_warnings),
+        .warnings = nice_warnings,
     };
 
-    int res;
-    if ((res = compile_w(info)))
+    int res = compile_w(info);
+    if (res)
         return res;
+
 
     if (contains("run", argc, argv)) {
         printf("./%s:\n", out);
+
+#ifdef _WIN32
+        return execute_w("%s.exe", out);
+#else
         return execute_w("./%s", out);
+#endif // _WIN32
     }
 
     return 0;
